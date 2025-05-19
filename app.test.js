cea188e8fc8796e77747a050a16ad33d1a651ec4
martin <ripmyloven@gmail.com>
2025-05-19 10:33:11 +0000

    testadd

--- app.test.js
+++ app.test.js
@@ -1,5 +1,5 @@
 const { hello } = require('./app.js');
 
 test('Film function should return Minu lemmik reisisihtkoht on Jaapan.', () => {
-expect(hello("Alien")).toBe('Minu lemmik reisisihtkoht on Jaapan.');
+  expect(hello("Jaapan")).toBe('Minu lemmik reisisihtkoht on Jaapan.');
 });
